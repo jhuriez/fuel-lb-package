9f7f5969fc966ae40e72f6f1b49b35cab39299b1
jhuriez <syntaxlb@gmail.com>
2014-09-03 09:40:32 +0200

    [ADD] Backend::getBtn function

--- classes/lb/backend.php
+++ classes/lb/backend.php
@@ -32,4 +32,72 @@ Class Backend
         }
     }
 
+
+    public static function getBtn($type, $url, $options = array())
+    {
+        // Default options
+        $useTooltip = (isset($options['use_tooltip'])) ? $options['use_tooltip'] : true;
+        $title = (isset($options['title'])) ? $options['title'] : false;
+        $id = (isset($options['id'])) ? $options['id'] : false;
+        $moreClasses = (isset($options['class'])) ? $options['class'] : '';
+        $attr = (isset($options['attr'])) ? $options['attr'] : array();
+
+        $res = '<a href="'.$url.'"';
+
+        // By type
+        $class = 'btn btn-circle';
+        switch($type)
+        {
+            case 'add':
+                $class .= ' btn-success';
+                ! $title and $title = 'Ajouter';
+                $icon = 'fa-plus';
+
+                break;
+
+            case 'edit':
+                $class .= ' btn-warning';
+                ! $title and $title = 'Modifier';
+                $icon = 'fa-pencil';
+                break;
+
+            case 'delete':
+                $class .= ' btn-danger';
+                ! $title and $title = 'Supprimer';
+                $icon = 'fa-trash-o';
+                break;
+
+            case 'view':
+                $class .= ' btn-info';
+                ! $title and $title = 'Voir';
+                $icon = 'fa-search';
+                break;
+        }
+
+        // Link class
+        $class .= ' '.$moreClasses;
+        $res .= ' class="'.$class.'"';
+
+        // Id
+        $id and $res .= ' id="'.$id.'"';
+
+        // Tooltip
+        $useTooltip and $res .= ' data-toggle="tooltip"';
+
+        // Title
+        $title and $res .= ' title="'.$title.'"';
+
+        // Attr
+        foreach($attr as $k => $v)
+        {
+            $res .= ' '.$k.'="'.$v.'"';
+        }
+
+        // Icon
+        $icon = '<i class="fa fa-lg '.$icon.'"></i>';
+
+        $res .= '>'.$icon.'</a>';
+
+        return $res;
+    }
 }
